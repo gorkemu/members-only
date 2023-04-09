ab67fdf14ac0796fa9cefc4906485cc613a07201
gorkemu <gorkemunal88@gmail.com>
2023-04-09 07:15:19 +0300

    add login, logout, create-message routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,28 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
 const authController = require("../controllers/authController");
+const messageController = require("../controllers/messageController");
 
 /* GET home page. */
 router.get("/", indexController.index);
+
+/* GET Login */
+router.get("/login", authController.login_get);
+/* POST Login */
+router.post("/login", authController.login_post);
 
 /* GET Sign Up */
 router.get("/sign-up", authController.sign_up_get);
 /* POST Sign Up */
 router.post("/sign-up", authController.sign_up_post);
+
+/* GET Log Out */
+router.get("/logout", authController.logout_get);
+
+/* GET Create Message */
+router.get("/create-message", messageController.create_message_get);
+
+/* POST Create Message */
+router.post("/create-message", messageController.create_message_post);
 
 module.exports = router;
