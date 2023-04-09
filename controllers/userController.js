c9d799c4030bdaccdbdc117308d414b03e67ca0a
gorkemu <gorkemunal88@gmail.com>
2023-04-09 22:03:15 +0300

    add become member GET & POST controllers

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const User = require("../models/userModel");
+const { body, validationResult } = require("express-validator");
+
+exports.become_member_get = (req, res, next) => {
+  if (!res.locals.currentUser) res.redirect("/login");
+  res.render("become-member", { title: "Become Member" });
+};
+
+exports.become_member_post = [
+  body("member_code", "Member code is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("become-member", {
+        title: "Become Member",
+        errors: errors.array(),
+      });
+      return;
+    }
+    try {
+      const user = await User.findById(req.user._id);
+      if (req.body.member_code === process.env.MEMBER_CODE) {
+        user.member = true;
+        await user.save();
+        res.redirect("/");
+      }
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
