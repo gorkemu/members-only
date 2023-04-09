30d201a9b01665b8e969ef47a889691bdf3dd17a
gorkemu <gorkemunal88@gmail.com>
2023-04-10 02:09:21 +0300

    redirect to login page if user is not logged in

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,7 @@
 const Message = require("../models/messageModel");
 
 exports.index = async (req, res, next) => {
+  if (!res.locals.currentUser) return res.redirect("/login");
   try {
     const messages = await Message.find()
       .populate("user")
